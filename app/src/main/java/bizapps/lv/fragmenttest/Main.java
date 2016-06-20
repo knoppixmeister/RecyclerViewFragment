47ab32bf339acf048b2b9ca3a84e2da7ce7a46ab
knoppixmeister <knoppixmeister@gmail.com>
2016-06-20 12:04:00 +0300

    work commit

--- app/src/main/java/bizapps/lv/fragmenttest/Main.java
+++ app/src/main/java/bizapps/lv/fragmenttest/Main.java
@@ -28,6 +28,7 @@ public class Main extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        /*
         int id = item.getItemId();
 
         if(id == R.id.action_settings) {
@@ -38,6 +39,7 @@ public class Main extends AppCompatActivity {
 
             return true;
         }
+        */
 
         return super.onOptionsItemSelected(item);
     }
