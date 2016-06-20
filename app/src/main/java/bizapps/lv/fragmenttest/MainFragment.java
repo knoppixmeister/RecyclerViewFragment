b0b5711f81a2a382ab91525aded7bc1048f0dc19
knoppixmeister <knoppixmeister@gmail.com>
2016-06-20 12:41:48 +0300

    work commit

--- app/src/main/java/bizapps/lv/fragmenttest/MainFragment.java
+++ app/src/main/java/bizapps/lv/fragmenttest/MainFragment.java
@@ -22,9 +22,7 @@ public class MainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
