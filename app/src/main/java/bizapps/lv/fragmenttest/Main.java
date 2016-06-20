3fd31afe5a682fceef02b979fd34e9e9d3cb6e98
knoppixmeister <knoppixmeister@gmail.com>
2016-06-20 12:53:10 +0300

    work commit

--- app/src/main/java/bizapps/lv/fragmenttest/Main.java
+++ app/src/main/java/bizapps/lv/fragmenttest/Main.java
@@ -1,11 +1,27 @@
 package bizapps.lv.fragmenttest;
 
-import android.os.Bundle;
+import android.content.*;
+import android.os.*;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.*;
+import com.android.vending.billing.IInAppBillingService;
 
 public class Main extends AppCompatActivity {
+    IInAppBillingService mService;
+
+    ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = IInAppBillingService.Stub.asInterface(service);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +33,10 @@ public class Main extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                                     .add(R.id.fragment_place, new MainFragment())
                                     .commit();
+
+        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
     }
 
     @Override
